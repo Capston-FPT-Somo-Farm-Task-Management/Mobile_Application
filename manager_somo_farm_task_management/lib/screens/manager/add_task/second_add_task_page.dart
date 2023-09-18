5f93d56ab3badd51bee6d1ab99c2730ccd29939a
HNGB <huynhngogiabao2211@gmail.com>
2023-09-18 19:39:53 +0700

    change to chip input, delete unnecessary

--- manager_somo_farm_task_management/lib/screens/manager/add_task/second_add_task_page.dart
+++ manager_somo_farm_task_management/lib/screens/manager/add_task/second_add_task_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chips_input/flutter_chips_input.dart';
 import 'package:manager_somo_farm_task_management/componets/constants.dart';
 import 'package:manager_somo_farm_task_management/models/employee.dart';
 import 'package:manager_somo_farm_task_management/models/employee_task_type.dart';
+import 'package:manager_somo_farm_task_management/models/material.dart';
 import 'package:manager_somo_farm_task_management/models/task_type.dart';
 import 'package:manager_somo_farm_task_management/screens/manager/add_task/componets/input_field.dart';
 import 'package:manager_somo_farm_task_management/screens/manager/add_task/third_add_task_page.dart';
@@ -18,7 +20,7 @@ class _SecondAddTaskPage extends State<SecondAddTaskPage> {
   final TextEditingController _noteController = TextEditingController();
 
   String _selectedTaskType = listTaskTypes[0].taskTypeName;
-  String _selectedEmployee = listEmployees[0].fullName;
+  Key _keyChange = UniqueKey();
   List<Employee> filteredEmployees = listEmployeeTaskTypes
       .where((employeeTaskType) => employeeTaskType.taskTypeId == 1)
       .map((employeeTaskType) => listEmployees.firstWhere(
@@ -27,7 +29,6 @@ class _SecondAddTaskPage extends State<SecondAddTaskPage> {
   List<String> user = ["Ronaldo", "Messi", "Benzema", "Mac Hai"];
   String _selectedUser = "Ronaldo";
 
-  int _selectedColor = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -93,8 +94,8 @@ class _SecondAddTaskPage extends State<SecondAddTaskPage> {
                           .toList();
 
                       // Gọi setState để cập nhật danh sách người thực hiện
-                      _selectedEmployee = filteredEmployees[0]
-                          .fullName; // Đặt lại người thực hiện khi thay đổi loại nhiệm vụ
+                      _keyChange =
+                          UniqueKey(); // Đặt lại người thực hiện khi thay đổi loại nhiệm vụ
                     });
                   },
                   items: listTaskTypes
@@ -106,30 +107,77 @@ class _SecondAddTaskPage extends State<SecondAddTaskPage> {
                   }).toList(),
                 ),
               ),
-              MyInputField(
-                title: "Người thực hiện",
-                hint: _selectedEmployee,
-                widget: DropdownButton(
-                  underline: Container(height: 0),
-                  icon: const Icon(
-                    Icons.keyboard_arrow_down,
-                    color: Colors.grey,
-                  ),
-                  iconSize: 32,
-                  elevation: 4,
-                  style: subTitileStyle,
-                  onChanged: (Employee? newValue) {
-                    setState(() {
-                      _selectedUser = newValue!.fullName;
-                    });
-                  },
-                  items: filteredEmployees
-                      .map<DropdownMenuItem<Employee>>((Employee value) {
-                    return DropdownMenuItem<Employee>(
-                      value: value,
-                      child: Text(value.fullName),
-                    );
-                  }).toList(),
+              Container(
+                margin: const EdgeInsets.only(top: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Người thực hiện", // Thay đổi tiêu đề nếu cần thiết
+                      style:
+                          titileStyle, // Đảm bảo bạn đã định nghĩa titileStyle
+                    ),
+                    Container(
+                      height: 52,
+                      margin: const EdgeInsets.only(top: 8.0),
+                      padding: const EdgeInsets.only(left: 14),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.grey,
+                          width: 1.0,
+                        ),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: SingleChildScrollView(
+                        child: ChipsInput(
+                          suggestionsBoxMaxHeight: 200,
+                          key: _keyChange,
+                          decoration: const InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Chọn người thực hiện",
+                              hintStyle: TextStyle(color: Colors.black45)),
+                          initialValue: [],
+                          findSuggestions: (String query) {
+                            if (query.length != 0) {
+                              var lowercaseQuery = query.toLowerCase();
+                              return filteredEmployees.where((e) {
+                                return e.fullName
+                                    .toLowerCase()
+                                    .contains(query.toLowerCase());
+                              }).toList(growable: false)
+                                ..sort((a, b) => a.fullName
+                                    .toLowerCase()
+                                    .indexOf(lowercaseQuery)
+                                    .compareTo(b.fullName
+                                        .toLowerCase()
+                                        .indexOf(lowercaseQuery)));
+                            } else {
+                              return const <Employee>[];
+                            }
+                          },
+                          onChanged: (data) {
+                            print(data);
+                          },
+                          chipBuilder: (context, state, employee) {
+                            return InputChip(
+                              key: ObjectKey(employee),
+                              label: Text(employee.fullName),
+                              onDeleted: () => state.deleteChip(employee),
+                              materialTapTargetSize:
+                                  MaterialTapTargetSize.shrinkWrap,
+                            );
+                          },
+                          suggestionBuilder: (context, state, profile) {
+                            return ListTile(
+                              key: ObjectKey(profile),
+                              title: Text(profile.fullName),
+                              onTap: () => state.selectSuggestion(profile),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
               MyInputField(
@@ -157,35 +205,82 @@ class _SecondAddTaskPage extends State<SecondAddTaskPage> {
                   }).toList(),
                 ),
               ),
+              Container(
+                margin: const EdgeInsets.only(top: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Dụng cụ", // Thay đổi tiêu đề nếu cần thiết
+                      style:
+                          titileStyle, // Đảm bảo bạn đã định nghĩa titileStyle
+                    ),
+                    Container(
+                      height: 52,
+                      margin: const EdgeInsets.only(top: 8.0),
+                      padding: const EdgeInsets.only(left: 14),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.grey,
+                          width: 1.0,
+                        ),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: SingleChildScrollView(
+                        child: ChipsInput(
+                          suggestionsBoxMaxHeight: 200,
+                          decoration: const InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Chọn dụng cụ cần thiết",
+                              hintStyle: TextStyle(color: Colors.black45)),
+                          initialValue: [],
+                          findSuggestions: (String query) {
+                            if (query.length != 0) {
+                              var lowercaseQuery = query.toLowerCase();
+                              return materials.where((m) {
+                                return m.materialName
+                                    .toLowerCase()
+                                    .contains(query.toLowerCase());
+                              }).toList(growable: false)
+                                ..sort((a, b) => a.materialName
+                                    .toLowerCase()
+                                    .indexOf(lowercaseQuery)
+                                    .compareTo(b.materialName
+                                        .toLowerCase()
+                                        .indexOf(lowercaseQuery)));
+                            } else {
+                              return const <Employee>[];
+                            }
+                          },
+                          onChanged: (data) {
+                            print(data);
+                          },
+                          chipBuilder: (context, state, material) {
+                            return InputChip(
+                              key: ObjectKey(material),
+                              label: Text(material.materialName),
+                              onDeleted: () => state.deleteChip(material),
+                              materialTapTargetSize:
+                                  MaterialTapTargetSize.shrinkWrap,
+                            );
+                          },
+                          suggestionBuilder: (context, state, profile) {
+                            return ListTile(
+                              key: ObjectKey(profile),
+                              title: Text(profile.materialName),
+                              onTap: () => state.selectSuggestion(profile),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
               MyInputField(
                 title: "Mô tả",
                 hint: "Nhập mô tả",
                 controller: _noteController,
-              ),
-              MyInputField(
-                title: "Dụng cụ",
-                hint: _selectedUser,
-                widget: DropdownButton(
-                  underline: Container(height: 0),
-                  icon: const Icon(
-                    Icons.keyboard_arrow_down,
-                    color: Colors.grey,
-                  ),
-                  iconSize: 32,
-                  elevation: 4,
-                  style: subTitileStyle,
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      _selectedUser = newValue!;
-                    });
-                  },
-                  items: user.map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                ),
               ),
               const SizedBox(height: 18),
               Row(
