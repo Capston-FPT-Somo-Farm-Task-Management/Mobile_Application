ea259931f0a9a70ecdcf2eb7d89d3f0832a504d3
HNGB <huynhngogiabao2211@gmail.com>
2023-09-19 16:26:14 +0700

    repeate details, use widget show snackbar

--- manager_somo_farm_task_management/lib/screens/manager/add_task/third_add_task_page.dart
+++ manager_somo_farm_task_management/lib/screens/manager/add_task/third_add_task_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:manager_somo_farm_task_management/componets/constants.dart';
+import 'package:manager_somo_farm_task_management/componets/snackBar.dart';
 import 'package:manager_somo_farm_task_management/screens/manager/add_task/componets/input_field.dart';
 
 class ThirdAddTaskPage extends StatefulWidget {
@@ -11,16 +12,17 @@ class ThirdAddTaskPage extends StatefulWidget {
 }
 
 class _ThirdAddTaskPage extends State<ThirdAddTaskPage> {
-  final TextEditingController _titleController = TextEditingController();
-  final TextEditingController _noteController = TextEditingController();
-  DateTime _selectedDate = DateTime.now();
-  String _endTime = "9:30 PM";
-  String _startTime = DateFormat("hh:mm a").format(DateTime.now()).toString();
+  DateTime? _selectedDate;
+  DateTime? _selectedDateRepeatUntil;
+  String _endTime = "hh:mm a";
+  String _startTime = "hh:mm a";
   int _selectedRemind = 5;
   List<int> remindList = [5, 10, 15, 20];
   String _selectedRepeat = "Không";
   List<String> repeatList = ["Không", "Hàng ngày", "Hàng tuần", "Hàng tháng"];
-
+  String showInputFieldRepeat = "Không";
+  List<int> repeatNumbers = [];
+  int _selectedRepeatNumber = 1;
   int _selectedColor = 0;
   @override
   Widget build(BuildContext context) {
@@ -56,14 +58,16 @@ class _ThirdAddTaskPage extends State<ThirdAddTaskPage> {
               ),
               MyInputField(
                 title: "Ngày thực hiện",
-                hint: DateFormat('dd/MM/yyyy').format(_selectedDate),
+                hint: _selectedDate == null
+                    ? "dd/MM/yyy"
+                    : DateFormat('dd/MM/yyyy').format(_selectedDate!),
                 widget: IconButton(
                   icon: const Icon(
                     Icons.calendar_today_outlined,
                     color: Colors.grey,
                   ),
                   onPressed: () {
-                    _getDateFromUser();
+                    _getDateFromUser(false);
                   },
                 ),
               ),
@@ -142,6 +146,12 @@ class _ThirdAddTaskPage extends State<ThirdAddTaskPage> {
                   onChanged: (String? newValue) {
                     setState(() {
                       _selectedRepeat = newValue!;
+                      showInputFieldRepeat = newValue;
+                      if (showInputFieldRepeat != "Không") {
+                        for (int i = 1; i <= 30; i++) {
+                          repeatNumbers.add(i);
+                        }
+                      }
                     });
                   },
                   items:
@@ -158,6 +168,59 @@ class _ThirdAddTaskPage extends State<ThirdAddTaskPage> {
                   }).toList(),
                 ),
               ),
+              if (showInputFieldRepeat != "Không")
+                MyInputField(
+                  title: "Lặp mỗi",
+                  hint: "$_selectedRepeatNumber",
+                  widget: DropdownButton(
+                    underline: Container(height: 0),
+                    icon: const Icon(
+                      Icons.keyboard_arrow_down,
+                      color: Colors.grey,
+                    ),
+                    iconSize: 32,
+                    elevation: 4,
+                    style: subTitileStyle,
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _selectedRepeatNumber = int.parse(newValue!);
+                      });
+                    },
+                    items: repeatNumbers
+                        .map<DropdownMenuItem<String>>((int value) {
+                      return DropdownMenuItem<String>(
+                        value: value.toString(),
+                        child: Text(value.toString()),
+                      );
+                    }).toList(),
+                    menuMaxHeight: 200,
+                  ),
+                ),
+              if (showInputFieldRepeat != "Không")
+                Container(
+                  margin: const EdgeInsets.only(top: 7.0),
+                  child: Text(
+                    showInputFieldRepeat.split(' ').last,
+                    style: titileStyle,
+                  ),
+                ),
+              if (showInputFieldRepeat != "Không")
+                MyInputField(
+                  title: "Lặp đến ngày",
+                  hint: _selectedDateRepeatUntil == null
+                      ? "dd/MM/yyy"
+                      : DateFormat('dd/MM/yyyy')
+                          .format(_selectedDateRepeatUntil!),
+                  widget: IconButton(
+                    icon: const Icon(
+                      Icons.calendar_today_outlined,
+                      color: Colors.grey,
+                    ),
+                    onPressed: () {
+                      _getDateFromUser(true);
+                    },
+                  ),
+                ),
               const SizedBox(height: 18),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -232,74 +295,89 @@ class _ThirdAddTaskPage extends State<ThirdAddTaskPage> {
   }
 
   _validateDate() {
-    if (_titleController.text.isNotEmpty && _noteController.text.isNotEmpty) {
+    if (_selectedDate != null &&
+        _startTime != "hh:mm a" &&
+        _endTime != "hh:mm a" &&
+        _selectedRepeat == "Không") {
       //add database
+      Navigator.of(context).pop();
+    } else if (_selectedRepeat != "Không" && _selectedDateRepeatUntil != null) {
       Navigator.of(context).pop();
     } else {
       // Nếu có ô trống, hiển thị Snackbar với biểu tượng cảnh báo và màu đỏ
-      const snackBar = SnackBar(
-        content: Row(
-          children: [
-            Icon(
-              Icons.error,
-              color: Colors.red, // Màu của biểu tượng cảnh báo
-            ),
-            SizedBox(width: 8), // Khoảng cách giữa biểu tượng và nội dung
-            Text(
-              'Vui lòng điền đầy đủ thông tin',
-              style: TextStyle(
-                color: Colors.red, // Màu của nội dung
-              ),
-            ),
-          ],
-        ),
-        backgroundColor: Colors.white, // Màu nền của Snackbar
-      );
-
-      // Hiển thị Snackbar
-      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      SnackbarShowNoti.showSnackbar(
+          context, 'Vui lòng điền đầy đủ thông tin', true);
     }
   }
 
-  _getDateFromUser() async {
+  _getDateFromUser(bool repeatUntilDate) async {
     DateTime? _pickerDate = await showDatePicker(
       context: context,
       initialDate: DateTime.now(),
-      firstDate: DateTime.now().subtract(const Duration(days: 1)),
+      firstDate: DateTime.now(),
       lastDate: DateTime.now().add(const Duration(days: 36525)),
     );
-    if (_pickerDate != null) {
-      setState(() {
-        _selectedDate = _pickerDate;
-      });
-    } else {
+    if (_pickerDate == null) {
       print("it's null or something is wrong");
+      return;
+    }
+    if (repeatUntilDate && _selectedDate == null) {
+      SnackbarShowNoti.showSnackbar(context, "Chọn ngày thực hiện trước", true);
+    } else if (repeatUntilDate && _pickerDate.isBefore(_selectedDate!)) {
+      SnackbarShowNoti.showSnackbar(
+          context, "Ngày kết thúc lặp lại phải lớn hơn ngày thực hiện", true);
+    } else {
+      setState(() {
+        if (repeatUntilDate) {
+          _selectedDateRepeatUntil = _pickerDate;
+        } else {
+          _selectedDate = _pickerDate;
+          _selectedDateRepeatUntil = null;
+        }
+      });
     }
   }
 
   _getTimeFromUser({required bool isStartTime}) async {
     var pickerTime = await _showTimePicker();
-    String _formatedTime = pickerTime.format(context);
+
     if (pickerTime == null) {
-      print('Time canceld');
-    } else if (isStartTime) {
+      print('Time canceled');
+      return;
+    }
+
+    String _formattedTime = pickerTime.format(context);
+
+    if (isStartTime) {
       setState(() {
-        _startTime = _formatedTime;
+        _startTime = _formattedTime;
+        _endTime = "hh:mm a";
       });
-    } else if (!isStartTime) {
-      setState(() {
-        _endTime = _formatedTime;
-      });
+    } else if (_startTime == "hh:mm a") {
+      SnackbarShowNoti.showSnackbar(context, "Chọn giờ bắt đầu trước", true);
+    } else {
+      DateTime startTime = DateFormat('hh:mm a').parse(_startTime);
+      DateTime endTime = DateFormat('hh:mm a').parse(_formattedTime);
+
+      if (endTime.isAfter(startTime)) {
+        setState(() {
+          _endTime = _formattedTime;
+        });
+      } else {
+        SnackbarShowNoti.showSnackbar(
+            context, "Giờ kết thúc phải lớn hơn giờ bắt đầu", true);
+      }
     }
   }
 
   _showTimePicker() {
+    String time = DateFormat("hh:mm a").format(DateTime.now()).toString();
     return showTimePicker(
       initialEntryMode: TimePickerEntryMode.input,
       context: context,
       initialTime: TimeOfDay(
-        hour: int.parse(_startTime.split(":")[0]),
-        minute: int.parse(_startTime.split(":")[1].split(" ")[0]),
+        hour: int.parse(time.split(":")[0]),
+        minute: int.parse(time.split(":")[1].split(" ")[0]),
       ),
     );
   }
