97a0f61a9340fdaed6bf90f7beea8d9ef818bfb3
HNGB <huynhngogiabao2211@gmail.com>
2023-09-30 21:24:42 +0700

    delete hard data

--- manager_somo_farm_task_management/lib/models/task_type.dart
+++ manager_somo_farm_task_management/lib/models/task_type.dart
@@ -29,36 +29,3 @@ class TaskType {
     };
   }
 }
-
-List<TaskType> listTaskTypes = [
-  TaskType(
-    taskTypeId: 1,
-    taskTypeName: "Trồng trọt",
-    description: "Description for Trồng trọt",
-    status: true,
-  ),
-  TaskType(
-    taskTypeId: 2,
-    taskTypeName: "Chăn nuôi",
-    description: "Description for Chăn nuôi",
-    status: false,
-  ),
-  TaskType(
-    taskTypeId: 3,
-    taskTypeName: "Thú y",
-    description: "Description for Thú y",
-    status: true,
-  ),
-  TaskType(
-    taskTypeId: 4,
-    taskTypeName: "Đất đai",
-    description: "Description for Đất đai",
-    status: false,
-  ),
-  TaskType(
-    taskTypeId: 5,
-    taskTypeName: "Thủy sản",
-    description: "Description for Thủy sản",
-    status: true,
-  ),
-];
