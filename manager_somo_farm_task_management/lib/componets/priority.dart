a9e069fb583fcdca8a5019c108a5d04b646a668c
HNGB <huynhngogiabao2211@gmail.com>
2023-09-30 21:25:10 +0700

    change int to string

--- manager_somo_farm_task_management/lib/componets/priority.dart
+++ manager_somo_farm_task_management/lib/componets/priority.dart
@@ -1,34 +1,17 @@
 import 'package:flutter/material.dart';
 
 class Priority {
-  static String getPriority(int no) {
+  static Color getBGClr(String no) {
     switch (no) {
-      case 1:
-        return "Thấp nhất";
-      case 2:
-        return "Thấp";
-      case 3:
-        return "Trung bình";
-      case 4:
-        return "Cao";
-      case 5:
-        return "Cao nhất";
-      default:
-        return "Không xác định";
-    }
-  }
-
-  static Color getBGClr(int no) {
-    switch (no) {
-      case 1:
+      case "Thấp nhất":
         return const Color(0xFF277DA1);
-      case 2:
+      case "Thấp":
         return const Color(0xFF90BE6D);
-      case 3:
+      case "Trung bình":
         return const Color(0xFFF9C74F);
-      case 4:
+      case "Cao":
         return const Color(0xFFF3722C);
-      case 5:
+      case "Cao nhất":
         return const Color(0xFFF94144);
       default:
         return Colors.transparent;
