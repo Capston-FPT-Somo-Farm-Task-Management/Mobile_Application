03eaa920f7ed09aaf65931ce117f1f7da2eb4658
HNGB <huynhngogiabao2211@gmail.com>
2023-10-05 22:03:47 +0700

    zontype service

---
+++ manager_somo_farm_task_management/lib/services/zone_type_service.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:manager_somo_farm_task_management/componets/constants.dart';
+import 'package:http/http.dart' as http;
+
+class ZoneTypeService {
+  Future<List<Map<String, dynamic>>> getZonesType() async {
+    final String getZonesUrl = '$baseUrl/ZoneType';
+
+    final http.Response response = await http.get(
+      Uri.parse(getZonesUrl),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final List<Map<String, dynamic>> zones =
+          List<Map<String, dynamic>>.from(json.decode(response.body));
+      return zones;
+    } else {
+      throw Exception('Failed to get zoneType');
+    }
+  }
+}
