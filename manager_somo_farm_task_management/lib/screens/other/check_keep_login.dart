164d5ae2d2024a3fa431402134d056b50bccc9b3
HNGB <huynhngogiabao2211@gmail.com>
2023-10-15 21:12:49 +0700

    create init

--- manager_somo_farm_task_management/lib/screens/other/check_keep_login.dart
+++ manager_somo_farm_task_management/lib/screens/other/check_keep_login.dart
@@ -19,7 +19,22 @@ class CheckKeepLoginState extends State<CheckKeepLogin> {
   @override
   void initState() {
     super.initState();
-    checkLoginStatus();
+    init();
+  }
+
+  Future<void> init() async {
+    await checkLoginStatus();
+
+    // Kiểm tra farmId và xác định trang để chuyển đến.
+    if (farmId == null) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginPage()),
+      );
+    } else {
+      setState(
+          () {}); // Gọi setState để rebuild widget và hiển thị trang chính.
+    }
   }
 
   // Kiểm tra trạng thái đăng nhập từ SharedPreferences
@@ -37,9 +52,6 @@ class CheckKeepLoginState extends State<CheckKeepLogin> {
 
   @override
   Widget build(BuildContext context) {
-    if (farmId == null) {
-      return LoginPage();
-    }
     return Scaffold(
       body: StreamBuilder(
           stream: FirebaseAuth.instance.authStateChanges(),
@@ -50,10 +62,8 @@ class CheckKeepLoginState extends State<CheckKeepLogin> {
               //if(role == "Manager")
               // Nếu đã đăng nhập bằng Firebase Auth hoặc có thông tin từ SharedPreferences
               return ManagerHomePage(farmId: farmId!);
-            } else if (snapshot.hasError) {
-              return const Center(child: Text('Something went wrong!'));
             } else {
-              return LoginPage();
+              return const Center(child: Text('Something went wrong!'));
             }
           }),
     );
