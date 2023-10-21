953c5ee8fd9e4a22b27bd6f7abbf921edef84363
HNGB <huynhngogiabao2211@gmail.com>
2023-10-21 21:14:00 +0700

    createEvidence

--- manager_somo_farm_task_management/lib/services/evidence_service.dart
+++ manager_somo_farm_task_management/lib/services/evidence_service.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
+import 'dart:io';
 
+import 'package:dio/dio.dart';
 import 'package:manager_somo_farm_task_management/componets/constants.dart';
 import 'package:http/http.dart' as http;
 
@@ -37,6 +39,42 @@ class EvidenceService {
     } else {
       final Map<String, dynamic> data = json.decode(response.body);
       return Future.error(data['message']);
+    }
+  }
+
+  Future<bool> createEvidence(
+      int taskId, String description, List<File> images) async {
+    final String url = '$baseUrl/TaskEvidence/AddTaskEvidenceeWithImage';
+    Dio dio = Dio();
+
+    // Tạo FormData để chứa dữ liệu multipart
+    FormData formData = FormData();
+
+    // Thêm description vào FormData
+    formData.fields.add(MapEntry('description', description));
+    formData.fields.add(MapEntry('taskId', taskId.toString()));
+
+    // Thêm hình ảnh vào FormData
+    for (int i = 0; i < images.length; i++) {
+      formData.files.add(MapEntry(
+        'imageFile',
+        await MultipartFile.fromFile(images[i].path),
+      ));
+    }
+
+    try {
+      // Gửi request POST với FormData
+      Response response = await dio.post(url, data: formData);
+
+      // Kiểm tra status code
+      if (response.statusCode == 200) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      print('Error: $error');
+      return false;
     }
   }
 }
