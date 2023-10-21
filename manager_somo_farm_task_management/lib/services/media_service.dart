907e4473361a52af68c5438c1126288e3190f64a
HNGB <huynhngogiabao2211@gmail.com>
2023-10-21 21:13:50 +0700

    try catch

--- manager_somo_farm_task_management/lib/services/media_service.dart
+++ manager_somo_farm_task_management/lib/services/media_service.dart
@@ -2,26 +2,34 @@ import 'package:photo_manager/photo_manager.dart';
 
 class MediaServices {
   Future loadAlbums(RequestType requestType) async {
-    var permission = await PhotoManager.requestPermissionExtend();
-    List<AssetPathEntity> albumList = [];
+    try {
+      var permission = await PhotoManager.requestPermissionExtend();
+      List<AssetPathEntity> albumList = [];
 
-    if (permission.isAuth == true) {
-      albumList = await PhotoManager.getAssetPathList(
-        type: requestType,
-      );
-    } else {
-      PhotoManager.openSetting();
+      if (permission.isAuth == true) {
+        albumList = await PhotoManager.getAssetPathList(
+          type: requestType,
+        );
+      } else {
+        PhotoManager.openSetting();
+      }
+
+      return albumList;
+    } catch (e) {
+      throw Exception(e);
     }
-
-    return albumList;
   }
 
   Future loadAssets(AssetPathEntity selectedAlbum) async {
-    List<AssetEntity> assetList = await selectedAlbum.getAssetListRange(
-      start: 0,
-      // ignore: deprecated_member_use
-      end: selectedAlbum.assetCount,
-    );
-    return assetList;
+    try {
+      List<AssetEntity> assetList = await selectedAlbum.getAssetListRange(
+        start: 0,
+        // ignore: deprecated_member_use
+        end: selectedAlbum.assetCount,
+      );
+      return assetList;
+    } catch (e) {
+      throw Exception(e);
+    }
   }
 }
