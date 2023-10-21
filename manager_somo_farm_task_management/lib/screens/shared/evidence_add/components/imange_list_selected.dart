ca7a9446937b65cc62476c52c2878fb866b1db10
HNGB <huynhngogiabao2211@gmail.com>
2023-10-21 21:13:20 +0700

    _showFullSizeImage

--- manager_somo_farm_task_management/lib/screens/shared/evidence_add/components/imange_list_selected.dart
+++ manager_somo_farm_task_management/lib/screens/shared/evidence_add/components/imange_list_selected.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:manager_somo_farm_task_management/componets/constants.dart';
 import 'package:photo_manager/photo_manager.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:photo_view/photo_view_gallery.dart';
 import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 class ImageListSelectedPage extends StatefulWidget {
@@ -24,6 +28,58 @@ class _ImageListPageState extends State<ImageListSelectedPage> {
         _scrollController.jumpTo(index: widget.indexFocus);
       }
     });
+  }
+
+  void _showFullSizeImage(AssetEntity assetEntity) async {
+    List<File> files = [];
+
+    // Lấy danh sách các file từ danh sách asset
+    for (AssetEntity asset in widget.selectedAssetList) {
+      File? file = await asset.file;
+      if (file != null) {
+        files.add(file);
+      }
+    }
+
+    // Xác định vị trí của ảnh hiện tại trong danh sách
+    int initialIndex = widget.selectedAssetList.indexOf(assetEntity);
+
+    // Mở trang xem kích thước đầy đủ với khả năng lướt qua các ảnh
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => buildPhotoViewGallery(
+            files.map((file) => file.path).toList(), initialIndex),
+      ),
+    );
+  }
+
+  Widget buildPhotoViewGallery(List<String> imagePaths, int initialIndex) {
+    PageController _photoPageController =
+        PageController(initialPage: initialIndex);
+
+    return Scaffold(
+      body: GestureDetector(
+        onVerticalDragUpdate: (details) {
+          Navigator.pop(context);
+        },
+        child: PhotoViewGallery.builder(
+          itemCount: imagePaths.length,
+          builder: (context, index) {
+            return PhotoViewGalleryPageOptions(
+              imageProvider: FileImage(File(imagePaths[index])),
+              minScale: PhotoViewComputedScale.contained * 0.8,
+              maxScale: PhotoViewComputedScale.covered * 2,
+            );
+          },
+          scrollPhysics: ClampingScrollPhysics(),
+          backgroundDecoration: BoxDecoration(
+            color: Colors.black,
+          ),
+          pageController: _photoPageController,
+        ),
+      ),
+    );
   }
 
   @override
@@ -70,19 +126,24 @@ class _ImageListPageState extends State<ImageListSelectedPage> {
       child: Stack(
         children: [
           // Hiển thị hình ảnh
-          AssetEntityImage(
-            asset,
-            isOriginal: false,
-            thumbnailSize: const ThumbnailSize.square(1000),
-            fit: BoxFit.cover,
-            errorBuilder: (context, error, stackTrace) {
-              return const Center(
-                child: Icon(
-                  Icons.error,
-                  color: Colors.red,
-                ),
-              );
+          GestureDetector(
+            onTap: () {
+              _showFullSizeImage(asset);
             },
+            child: AssetEntityImage(
+              asset,
+              isOriginal: false,
+              thumbnailSize: const ThumbnailSize.square(1000),
+              fit: BoxFit.cover,
+              errorBuilder: (context, error, stackTrace) {
+                return const Center(
+                  child: Icon(
+                    Icons.error,
+                    color: Colors.red,
+                  ),
+                );
+              },
+            ),
           ),
           // Biểu tượng gỡ bỏ
           Positioned(
