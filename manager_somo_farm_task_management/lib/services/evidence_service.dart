e8e6c507c6f89b96ae321355ec1ab30a408e79aa
HNGB <huynhngogiabao2211@gmail.com>
2023-10-23 20:27:00 +0700

    getEvidencebyId

--- manager_somo_farm_task_management/lib/services/evidence_service.dart
+++ manager_somo_farm_task_management/lib/services/evidence_service.dart
@@ -27,6 +27,27 @@ class EvidenceService {
     }
   }
 
+  Future<Map<String, dynamic>> getEvidencebyId(int evidenceId) async {
+    final String getZonesUrl = '$baseUrl/TaskEvidence/$evidenceId';
+
+    final http.Response response = await http.get(
+      Uri.parse(getZonesUrl),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body);
+      final Map<String, dynamic> evidence =
+          Map<String, dynamic>.from(data['data']);
+      return evidence;
+    } else {
+      final Map<String, dynamic> data = json.decode(response.body);
+      return Future.error(data['message']);
+    }
+  }
+
   Future<bool> deleteEvidence(int evidenceId) async {
     final String url = '$baseUrl/TaskEvidence/${evidenceId}';
     final response = await http.delete(
@@ -65,6 +86,50 @@ class EvidenceService {
     try {
       // Gửi request POST với FormData
       Response response = await dio.post(url, data: formData);
+
+      // Kiểm tra status code
+      if (response.statusCode == 200) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      print('Error: $error');
+      return false;
+    }
+  }
+
+  Future<bool> updateEvidenceById(int evidenceId, int taskId,
+      String description, List<dynamic> urlImages, List<File> images) async {
+    final String url = '$baseUrl/TaskEvidence/${evidenceId}';
+    Dio dio = Dio();
+
+    // Tạo FormData để chứa dữ liệu multipart
+    FormData formData = FormData();
+
+    // Thêm description vào FormData
+    formData.fields
+        .add(MapEntry('evidenceCreateUpdateModel.description', description));
+    formData.fields
+        .add(MapEntry('evidenceCreateUpdateModel.taskId', taskId.toString()));
+
+    // Thêm hình ảnh vào FormData
+    for (int i = 0; i < images.length; i++) {
+      formData.files.add(MapEntry(
+        'evidenceCreateUpdateModel.imageFile',
+        await MultipartFile.fromFile(images[i].path),
+      ));
+    }
+    // Thêm đường dẫn hình ảnh vào FormData
+    for (int i = 0; i < urlImages.length; i++) {
+      formData.fields.add(MapEntry(
+        'oldUrlImage',
+        urlImages[i],
+      ));
+    }
+    try {
+      // Gửi request POST với FormData
+      Response response = await dio.put(url, data: formData);
 
       // Kiểm tra status code
       if (response.statusCode == 200) {
