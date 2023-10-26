6c07c85a389143e7b1d9cf7ea22df6ea4ad2de60
HNGB <huynhngogiabao2211@gmail.com>
2023-10-26 19:49:46 +0700

    isLoading

--- manager_somo_farm_task_management/lib/screens/shared/settings_page.dart
+++ manager_somo_farm_task_management/lib/screens/shared/settings_page.dart
@@ -1,107 +1,132 @@
 import 'package:flutter/material.dart';
 import 'package:manager_somo_farm_task_management/componets/constants.dart';
-import 'package:manager_somo_farm_task_management/services/google_authentication_service.dart';
+import 'package:manager_somo_farm_task_management/componets/snackBar.dart';
 import 'package:manager_somo_farm_task_management/services/hub_connection_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'login_page.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  bool isLoading = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white10,
-        elevation: 0,
-        automaticallyImplyLeading: false, // Ẩn nút quay lại
-        centerTitle: true,
-        title: Container(
-          margin: EdgeInsets.only(top: 10),
-          child: Text(
-            'Settings',
-            style: TextStyle(
-              fontSize: 25.0,
-              fontWeight: FontWeight.bold,
-              color: kPrimaryColor,
-            ),
-          ),
-        ),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            margin: const EdgeInsets.only(top: 80),
-            padding: const EdgeInsets.fromLTRB(30, 16, 30, 16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Account Settings',
+      appBar: isLoading
+          ? null
+          : AppBar(
+              backgroundColor: Colors.white10,
+              elevation: 0,
+              automaticallyImplyLeading: false, // Ẩn nút quay lại
+              centerTitle: true,
+              title: Container(
+                margin: EdgeInsets.only(top: 10),
+                child: Text(
+                  'Settings',
                   style: TextStyle(
-                    fontSize: 18.0,
+                    fontSize: 25.0,
                     fontWeight: FontWeight.bold,
+                    color: kPrimaryColor,
                   ),
                 ),
-                const SizedBox(height: 16.0),
-                ListTile(
-                  leading: const Icon(Icons.person),
-                  title: const Text('Profile'),
-                  onTap: () {},
+              ),
+            ),
+      body: isLoading
+          ? Center(
+              child: CircularProgressIndicator(color: kPrimaryColor),
+            )
+          : Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 80),
+                  padding: const EdgeInsets.fromLTRB(30, 16, 30, 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Account Settings',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 16.0),
+                      ListTile(
+                        leading: const Icon(Icons.person),
+                        title: const Text('Profile'),
+                        onTap: () {},
+                      ),
+                      ListTile(
+                        leading: const Icon(Icons.lock),
+                        title: const Text('Privacy'),
+                        onTap: () {},
+                      ),
+                    ],
+                  ),
                 ),
-                ListTile(
-                  leading: const Icon(Icons.lock),
-                  title: const Text('Privacy'),
-                  onTap: () {},
+                const SizedBox(height: 24.0),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(30, 16, 30, 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'General Settings',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 16.0),
+                      ListTile(
+                        leading: const Icon(Icons.notifications),
+                        title: const Text('Notifications'),
+                        onTap: () {},
+                      ),
+                      ListTile(
+                        leading: const Icon(Icons.language),
+                        title: const Text('Language'),
+                        onTap: () {},
+                      ),
+                      ListTile(
+                        leading: const Icon(Icons.output_outlined),
+                        title: const Text('Log out'),
+                        onTap: () async {
+                          // Điều hướng đến trang LoginPage
+                          setState(() {
+                            isLoading = true;
+                          });
+                          SharedPreferences prefs =
+                              await SharedPreferences.getInstance();
+                          final tokenDevice = prefs.getString('tokenDevice');
+                          await HubConnectionService()
+                              .deleteConnection(tokenDevice!)
+                              .then((value) {
+                            if (value) {
+                              setState(() {
+                                isLoading = false;
+                              });
+                              prefs.clear();
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => LoginPage()),
+                              );
+                            }
+                          }).catchError((e) {
+                            SnackbarShowNoti.showSnackbar(e.toString(), true);
+                          });
+                        },
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
-          ),
-          const SizedBox(height: 24.0),
-          Container(
-            padding: const EdgeInsets.fromLTRB(30, 16, 30, 16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'General Settings',
-                  style: TextStyle(
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 16.0),
-                ListTile(
-                  leading: const Icon(Icons.notifications),
-                  title: const Text('Notifications'),
-                  onTap: () {},
-                ),
-                ListTile(
-                  leading: const Icon(Icons.language),
-                  title: const Text('Language'),
-                  onTap: () {},
-                ),
-                ListTile(
-                  leading: const Icon(Icons.output_outlined),
-                  title: const Text('Log out'),
-                  onTap: () async {
-                    // Điều hướng đến trang LoginPage
-                    AuthService().logout();
-                    SharedPreferences prefs =
-                        await SharedPreferences.getInstance();
-                    final tokenDevice = prefs.getString('tokenDevice');
-                    await HubConnectionService().deleteConnection(tokenDevice!);
-                    await prefs.clear();
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context) => LoginPage()),
-                    );
-                  },
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
