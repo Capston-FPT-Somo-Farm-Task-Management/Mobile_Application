6c1797330466bc56378c112072a87f830b92b452
HuDery <duyhl0211@gmail.com>
2023-10-30 10:37:39 +0700

    create user profile page

--- manager_somo_farm_task_management/lib/screens/shared/settings_page.dart
+++ manager_somo_farm_task_management/lib/screens/shared/settings_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:manager_somo_farm_task_management/componets/constants.dart';
 import 'package:manager_somo_farm_task_management/componets/snackBar.dart';
+import 'package:manager_somo_farm_task_management/screens/manager/user/user_profile_page.dart';
 import 'package:manager_somo_farm_task_management/services/hub_connection_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'login_page.dart';
@@ -58,7 +59,11 @@ class _SettingsPageState extends State<SettingsPage> {
                       ListTile(
                         leading: const Icon(Icons.person),
                         title: const Text('Thông tin cá nhân'),
-                        onTap: () {},
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => UserProfilePage(),
+                          ));
+                        },
                       ),
                       ListTile(
                         leading: const Icon(Icons.lock),
