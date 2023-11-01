fcbe3403c33bc2378cf2885211f98f621bf37d18
HuDery <duyhl0211@gmail.com>
2023-11-01 22:04:33 +0700

    Fix UI User profile

--- manager_somo_farm_task_management/lib/screens/shared/user/user_profile_page.dart
+++ manager_somo_farm_task_management/lib/screens/shared/user/user_profile_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:manager_somo_farm_task_management/services/farm_service.dart';
+import 'package:intl/intl.dart';
+import 'package:manager_somo_farm_task_management/componets/constants.dart';
 import 'package:manager_somo_farm_task_management/services/user_services.dart';
 import 'package:manager_somo_farm_task_management/widgets/app_bar.dart';
 
@@ -36,36 +37,199 @@ class _UserProfilePageState extends State<UserProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(80),
-        child: CustomAppBar(),
+      appBar: AppBar(
+        backgroundColor: Colors.grey[200],
+        elevation: 0,
+        leading: GestureDetector(
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+            child: Icon(Icons.arrow_back, color: kSecondColor)),
       ),
       body: Container(
         color: Colors.grey[200],
-        padding: EdgeInsets.only(left: 15, right: 15, top: 30, bottom: 20),
+        padding: EdgeInsets.only(left: 15, right: 15, top: 5, bottom: 20),
         child: Column(
           children: [
             SizedBox(height: 30),
             Expanded(
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisSize: MainAxisSize.min,
                 children: [
+                  CircleAvatar(
+                    radius: 80,
+                    backgroundColor: Colors.grey,
+                    child: Icon(
+                      Icons.person,
+                      size: 100,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 25),
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Text(
                         userData != null ? userData!['name'] : '',
                         style: const TextStyle(
-                          fontSize: 20,
+                          fontSize: 23,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
                     ],
                   ),
+                  SizedBox(height: 25),
+                  Container(
+                    padding: EdgeInsets.all(30),
+                    height: 225,
+                    width: 350,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(45)),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            userData != null
+                                ? userData!['code'] != null
+                                    ? 'Mã nhân viên: ${userData!['code']}'
+                                    : 'Mã nhân viên: chưa có'
+                                : '',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            userData != null
+                                ? userData!['roleName'] != null
+                                    ? 'Chức vụ: ${userData!['roleName']}'
+                                    : 'Chức vụ: chưa có'
+                                : '',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            userData != null
+                                ? userData!['email'] != null
+                                    ? 'Email: ${userData!['email']}'
+                                    : 'Mã nhân viên: chưa có'
+                                : '',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            userData != null
+                                ? userData!['userName'] != null
+                                    ? 'Tên tài khoản: ${userData!['userName']}'
+                                    : 'Tên tài khoản: chưa có'
+                                : '',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            userData != null
+                                ? userData!['password'] != null
+                                    ? 'Mật khẩu: ${'**' * userData!['password'].toString().length}'
+                                    : 'Mật khẩu: chưa có'
+                                : '',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    padding: EdgeInsets.all(30),
+                    height: 200,
+                    width: 350,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(45)),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            userData != null
+                                ? userData!['phoneNumber'] != null
+                                    ? 'Số điện thoại: ${userData!['phoneNumber']}'
+                                    : 'Số điện thoại: chưa có'
+                                : '',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            userData != null
+                                ? userData!['birthday'] != null
+                                    ? 'Ngày sinh: ${DateFormat('dd/MM/yyyy').format(DateTime.parse(userData!['birthday']))}'
+                                    : 'Ngày sinh: chưa có'
+                                : '',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            userData != null
+                                ? userData!['address'] != null
+                                    ? 'Địa chỉ: ${userData!['address']}'
+                                    : 'Địa chỉ: chưa có'
+                                : '',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
                 ],
               ),
-            )
+            ),
           ],
         ),
       ),
