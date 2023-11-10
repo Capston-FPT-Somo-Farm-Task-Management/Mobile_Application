e6bdf4afda4bc58ab9cd35ae4fc408ed8fae2634
HNGB <huynhngogiabao2211@gmail.com>
2023-11-10 22:00:02 +0700

    change case 0

--- manager_somo_farm_task_management/lib/screens/shared/dashboard/dashboard_page.dart
+++ manager_somo_farm_task_management/lib/screens/shared/dashboard/dashboard_page.dart
@@ -398,25 +398,25 @@ Widget getBottomTitles(double value, TitleMeta meta) {
   late Widget text;
   switch (value.toInt()) {
     case 0:
-      text = const Text('T2', style: style);
+      text = const Text('CN', style: style);
       break;
     case 1:
-      text = const Text('T3', style: style);
+      text = const Text('T2', style: style);
       break;
     case 2:
-      text = const Text('T4', style: style);
+      text = const Text('T3', style: style);
       break;
     case 3:
-      text = const Text('T5', style: style);
+      text = const Text('T4', style: style);
       break;
     case 4:
-      text = const Text('T6', style: style);
+      text = const Text('T5', style: style);
       break;
     case 5:
-      text = const Text('T7', style: style);
+      text = const Text('T6', style: style);
       break;
     case 6:
-      text = const Text('CN', style: style);
+      text = const Text('T7', style: style);
       break;
   }
   return SideTitleWidget(child: text, axisSide: meta.axisSide);
