23f20ef8e4e4cfc9fa8cc58f5893f55021d6ff44
HuDery <duyhl0211@gmail.com>
2023-11-12 20:53:25 +0700

    Change UI Appbar AreaPage

--- manager_somo_farm_task_management/lib/screens/manager/area/area_page.dart
+++ manager_somo_farm_task_management/lib/screens/manager/area/area_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:manager_somo_farm_task_management/componets/alert_dialog_confirm.dart';
 import 'package:manager_somo_farm_task_management/componets/constants.dart';
+import 'package:manager_somo_farm_task_management/componets/hamburger_show_menu.dart';
 import 'package:manager_somo_farm_task_management/componets/snackBar.dart';
 import 'package:manager_somo_farm_task_management/screens/manager/area_add/area_add.dart';
 import 'package:manager_somo_farm_task_management/screens/manager/area_detail/area_detail_popup.dart';
@@ -66,30 +67,40 @@ class AreaPageState extends State<AreaPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const PreferredSize(
-        preferredSize: Size.fromHeight(80),
-        child: CustomAppBar(),
+      appBar: AppBar(
+        toolbarHeight: 70,
+        backgroundColor: Colors.grey[200],
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+        title: Text(
+          'Khu vực',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 25.0,
+            fontWeight: FontWeight.bold,
+            color: kPrimaryColor,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.menu),
+            color: Colors.black,
+            iconSize: 35,
+            onPressed: () {
+              HamburgerMenu.showReusableBottomSheet(context);
+            },
+          ),
+        ],
       ),
       body: Container(
         color: Colors.grey[200],
-        padding:
-            const EdgeInsets.only(left: 20, right: 20, top: 30, bottom: 20),
+        padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
         child: Column(
           children: [
             SingleChildScrollView(
               child: Column(
                 children: [
-                  const Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      "Khu vực",
-                      style: TextStyle(
-                        fontSize: 28, // Thay đổi kích thước phù hợp
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 10),
                   Align(
                     alignment: Alignment.centerLeft,
                     child: Row(
